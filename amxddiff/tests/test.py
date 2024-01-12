edec02d3476233a7c7e78b2ff0e7b5f75718171c
mkp <mkp@ableton.com>
2024-01-12 11:26:48 +0100

    amxddiff: test: separate baseline for zipped .als
    So that the zipped file doesn’t have to be changed when changing things to the main (unzipped) Test.als

--- amxddiff/tests/test.py
+++ amxddiff/tests/test.py
@@ -57,8 +57,8 @@ class TestStringMethods(unittest.TestCase):
     def test_parse_als_zipped(self):
         self.maxDiff = None
 
-        expected_file_path = get_test_path_file("test_baselines/Test.als.txt")
-        test_file_path = get_test_path_file("test_files/Test Project/Test.als")
+        expected_file_path = get_test_path_file("test_baselines/Zipped.als.txt")
+        test_file_path = get_test_path_file("test_files/Test Project/Zipped.als")
 
         with open(expected_file_path, mode="r") as expected_file:
             expected = expected_file.read()
@@ -70,7 +70,7 @@ class TestStringMethods(unittest.TestCase):
 
         # result of an unzipped set should be same as a zipped set
         expected_file_path = get_test_path_file("test_baselines/Test.als.txt")
-        test_file_path = get_test_path_file("test_files/Test Project/Test-NoZip.als")
+        test_file_path = get_test_path_file("test_files/Test Project/Test.als")
 
         with open(expected_file_path, mode="r") as expected_file:
             expected = expected_file.read()
