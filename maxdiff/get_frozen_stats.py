b1812db2a326f6259d350a49ed6c71eb7d5c3877
Mattijs Kneppers <mattijskneppers@client3900.office.ableton.com>
2024-07-04 13:40:00 +0200

    Add statistics: unique object instances and connections
    # Conflicts:
    #	maxdiff/get_frozen_stats.py

--- maxdiff/get_frozen_stats.py
+++ maxdiff/get_frozen_stats.py
@@ -5,7 +5,7 @@ from freezing_utils import device_entry_with_data
 def get_frozen_stats(entries: list[device_entry_with_data]):
     """Returns statistics for this device"""
 
-    device_data = entries[0]["data"]  # the first entry is always the device file
+    device = entries[0]  # the first entry is always the device file
 
     abstractions = [
         item for item in entries if str(item["file_name"]).endswith(".maxpat")
@@ -14,22 +14,34 @@ def get_frozen_stats(entries: list[device_entry_with_data]):
     # cache names of known abstractions
     file_names = [str(item["file_name"]) for item in abstractions]
 
-    patch = get_patcher_dict(device_data)
-    object_count, line_count = count_recursive(patch, abstractions, file_names)
+    device_patch = get_patcher_dict(device)
+    object_count_recursive, line_count_recursive = count(
+        device_patch, abstractions, file_names
+    )
 
     summary = "\n"
     summary += "Total - Counting every abstraction instance - Indicates loading time\n"
-    summary += f"    Object instances: {object_count}\n"
-    summary += f"    Connections: {line_count}\n"
+    summary += f"    Object instances: {object_count_recursive}\n"
+    summary += f"    Connections: {line_count_recursive}\n"
+
+    object_count_once = 0
+    line_count_once = 0
+    for entry in entries:
+        if entry["type"] != "JSON":
+            continue
+
+        entry_patch = get_patcher_dict(entry)
+        o, l = count(entry_patch, [], [])  # don't recurse into abstractions
+        object_count_once += o
+        line_count_once += l
+
     summary += "Unique - Counting abstractions once - Indicates maintainability\n"
-    summary += "    Object instances:\n"
-    summary += "    Connections:\n"
+    summary += f"    Object instances: {object_count_once}\n"
+    summary += f"    Connections: {line_count_once}\n"
     return summary
 
 
-def count_recursive(
-    patcher, entries: list[dict], file_names: list[str]
-) -> tuple[int, int]:
+def count(patcher, entries: list[dict], file_names: list[str]) -> tuple[int, int]:
     """Recursively counts all object instances and connections in this patcher,
     inluding in every instance of its dependencies"""
     boxes = patcher["boxes"]
@@ -46,29 +58,34 @@ def count_recursive(
                 box.get("maxclass") == "bpatcher" and box.get("embed") == 1
             ):
                 # get subpatcher or embedded bpatcher count
-                o, l = count_recursive(patch, entries, file_names)
+                o, l = count(patch, entries, file_names)
                 object_count += o
                 line_count += l
+
+    if len(file_names) == 0:
+        return (object_count, line_count)
+
+    # recurse into abstractions
+    for box_entry in boxes:
+        box = box_entry["box"]
+
+        file_name = get_abstraction_name(box, file_names)
+        if file_name is None:
+            continue
+
+        abstraction = [item for item in entries if item["file_name"] == file_name][0]
+        abstraction_patch = get_patcher_dict(abstraction)
+        o, l = count(abstraction_patch, entries, file_names)
+
+        if "text" in box and box["text"].startswith("poly~"):
+            # get poly abstraction count
+            voice_count = int(box["text"].split(" ")[2])
+            object_count += o * voice_count
+            line_count += l * voice_count
         else:
-            abstraction_name = get_abstraction_name(box, file_names)
-            if abstraction_name is None:
-                continue
-
-            abstraction = [
-                item for item in entries if item["file_name"] == abstraction_name
-            ][0]
-            abstraction_patch = get_patcher_dict(abstraction["data"])
-            o, l = count_recursive(abstraction_patch, entries, file_names)
-
-            if "text" in box and box["text"].startswith("poly~"):
-                # get poly abstraction count
-                voice_count = int(box["text"].split(" ")[2])
-                object_count += o * voice_count
-                line_count += l * voice_count
-            else:
-                # get abstraction count
-                object_count += o
-                line_count += l
+            # get abstraction count
+            object_count += o
+            line_count += l
 
     return (object_count, line_count)
 
@@ -105,29 +122,43 @@ def get_abstraction_name(box, file_names: list[str]):
     return None
 
 
-def get_patcher_dict(patch_data):
+def get_patcher_dict(entry: device_entry_with_data):
     """Returns the dict that represents the given patcher data.
     Prints errors if parsing fails"""
-    device_data_text = ""
 
+    if not "data" in entry:
+        return {}
+
+    patch_data = entry["data"]
+    if not isinstance(patch_data, bytes):
+        return {}
+
+    if not "file_name" in entry:
+        return {}
+
+    name = entry["file_name"]
+    if not isinstance(name, str):
+        return {}
+
+    device_data_text = ""
     try:
         if patch_data[len(patch_data) - 1] == 0:
             device_data_text = patch_data[: len(patch_data) - 1].decode("utf-8")
         else:
             device_data_text = patch_data.decode("utf-8")
     except Exception as e:
-        print(f"Error getting device patch data as text: {e}")
+        print(f"Error getting patch data as text for entry {name}: {e}")
+        return {}
 
-    if device_data_text == "":
-        print("Device has no data")
-
-    patcher_dict = {}
     try:
         patcher_dict = json.loads(device_data_text)
     except ValueError as e:
-        print(f"Error parsing device patch data as json: {e}")
+        print(f"Error parsing device patch data as json for entry {name}: {e}")
+        return {}
 
-    if not "patcher" in patcher_dict:
-        print("Device content does not seem to be a patcher")
-
-    return patcher_dict["patcher"]
+    try:
+        patcher = patcher_dict["patcher"]
+        return patcher
+    except:
+        print(f"Content of entry {name} does not seem to be a patcher")
+        return {}
