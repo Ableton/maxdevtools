d0b47048f505273e4b25d2fbbb77221e1d5d020a
mkp <mkp@ableton.com>
2024-11-15 15:42:52 +0100

    Extract recursive patcher parsing into a separate module
    This makes it possible to parse patchers for different data, passing an implementation of a Processor class to store the data.

--- maxdiff/get_frozen_stats.py
+++ maxdiff/get_frozen_stats.py
@@ -1,4 +1,5 @@
 from freezing_utils import device_entry_with_data, get_patcher_dict
+from process_patcher import Processor, process_patch
 
 
 def get_frozen_stats(entries: list[device_entry_with_data]) -> tuple[int, int, int, int]:
@@ -10,118 +11,49 @@ def get_frozen_stats(entries: list[device_entry_with_data]) -> tuple[int, int, i
         item for item in entries if str(item["file_name"]).endswith(".maxpat")
     ]
 
-    # cache names of known abstractions
-    abstraction_filenames = [str(item["file_name"]) for item in abstraction_entries]
-
     device_patch = get_patcher_dict(device)
     if device_patch == {}:
         return 0, 0, 0, 0
 
-    object_count_recursive, line_count_recursive = count(
-        device_patch,
-        abstraction_entries,
-        abstraction_filenames,  # do recurse into abstractions
+    # get total counts: parse every instance of every abstraction
+    count_processor = CountProcessor()
+    process_patch(
+        device_patch, abstraction_entries, count_processor  # do recurse into abstractions
     )
+    object_count_total, line_count_total = count_processor.get_results()
 
-    object_count_once = 0
-    line_count_once = 0
+    # get unique counts: parse every entry included in the frozen device once
+    object_count_unique = 0
+    line_count_unique = 0
     for entry in entries:
-        if entry["type"] != "JSON":
+        filename = str(entry["file_name"])
+        if not (filename.endswith(".amxd") or filename.endswith(".maxpat")):
             continue
 
         entry_patch = get_patcher_dict(entry)
-        o, l = count(entry_patch, [], [])  # don't recurse into abstractions
-        object_count_once += o
-        line_count_once += l
-
-    return (
-        object_count_recursive,
-        line_count_recursive,
-        object_count_once,
-        line_count_once,
-    )
-
-
-def count(
-    patcher, abstractions: list[dict], abstraction_filenames: list[str]
-) -> tuple[int, int]:
-    """Recursively counts all object instances and connections in this patcher,
-    inluding in every instance of its dependencies that can be found among the
-    files that were passed in"""
-    boxes = patcher["boxes"]
-    lines = patcher["lines"]
-    object_count = len(boxes)
-    line_count = len(lines)
-
-    for box_entry in boxes:
-        box = box_entry["box"]
-
-        if "patcher" in box:
-            patch = box["patcher"]
-            if box.get("maxclass") != "bpatcher" or (
-                box.get("maxclass") == "bpatcher" and box.get("embed") == 1
-            ):
-                # get subpatcher or embedded bpatcher count
-                o, l = count(patch, abstractions, abstraction_filenames)
-                object_count += o
-                line_count += l
-
-    if len(abstraction_filenames) == 0:
-        return (object_count, line_count)
-
-    # recurse into abstractions
-    for box_entry in boxes:
-        box = box_entry["box"]
-
-        file_name = get_abstraction_name(box, abstraction_filenames)
-        if file_name is None:
+        if entry_patch == {}:
             continue
 
-        abstraction = [item for item in abstractions if item["file_name"] == file_name][0]
-        abstraction_patch = get_patcher_dict(abstraction)
-        o, l = count(abstraction_patch, abstractions, abstraction_filenames)
+        count_processor = CountProcessor()
+        process_patch(entry_patch, [], count_processor)  # don't recurse into abstractions
+        o, l = count_processor.get_results()
 
-        if "text" in box and box["text"].startswith("poly~"):
-            # get poly abstraction count
-            tokens = box["text"].split(" ")
-            voice_count = int(tokens[2]) if len(tokens) > 2 else 1
-            object_count += o * voice_count
-            line_count += l * voice_count
-        else:
-            # get abstraction count
-            object_count += o
-            line_count += l
+        object_count_unique += o
+        line_count_unique += l
 
-    return (object_count, line_count)
+    return object_count_total, line_count_total, object_count_unique, line_count_unique
 
 
-def get_abstraction_name(box, abstraction_filenames: list[str]):
-    """
-    Checks if this box is an abstraction and if so, return the name of the abstraction file.
-    - returns None if this is not an abstraction
-    - throws error if an abstraction name was expected but it was not found in the list of known names
-    """
-    if "text" in box:
-        if box["text"].startswith("poly~"):
-            name = box["text"].split(" ")[1] + ".maxpat"
-            if name in abstraction_filenames:
-                return name
-            else:
-                raise ValueError(
-                    "poly~ pointing to file that is not known as a dependency: " + name
-                )
-        else:
-            name = box["text"].split(" ")[0] + ".maxpat"
-            if name in abstraction_filenames:
-                return name
+class CountProcessor(Processor):
+    def __init__(self):
+        self.object_count = 0
+        self.line_count = 0
 
-    if box.get("maxclass") == "bpatcher" and box.get("embed") != 1:
-        if box.get("name") in abstraction_filenames:
-            return box["name"]
-        else:
-            raise ValueError(
-                "Non-embedded bpatcher pointing to file that is not known as a dependency: "
-                + box["name"]
-            )
+    def process_elements(self, patcher, voice_count: int, abstraction_name=""):
+        """Counts objects and lines in the given patcher."""
+        self.object_count += len(patcher.get("boxes", [])) * voice_count
+        self.line_count += len(patcher.get("lines", [])) * voice_count
 
-    return None
+    def get_results(self):
+        """Returns the current counts."""
+        return self.object_count, self.line_count
